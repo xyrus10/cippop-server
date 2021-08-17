8cccb0c8d3d69c0be67591dda166f0ea3ddafec0
xyrus10 <88962617+xyrus10@users.noreply.github.com>
2021-08-17 08:21:40 +0700

    aa

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-var express = require('express')
-var app = express()
-var http = require('http').createServer(app);
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app);
 app.use(express.static('public'));
+
+var data = "do shash'owania";
+var crypto = require('crypto');
+crypto.createHash('md5').update(data).digest("hex");
 
 http.listen(process.env.PORT || 6996, function () {
     var host = http.address().address
@@ -15,6 +19,11 @@ app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/index.html`)
 })
 
+function md5(text, loop = 1) {
+    var crypto = require('crypto');
+    return crypto.createHash('md5').update(text).digest("hex");
+}
+
 const io = require("socket.io")(http, {
     cors: {
         origin: '*',
@@ -24,36 +33,43 @@ const io = require("socket.io")(http, {
 
 clientList = []
 
+function getPlayerList(array) {
+    let arrayList = []
+    Object.keys(array).forEach((value, index) => {
+        arrayList.push(array[value])
+    })
+    arrayList.sort(function (a, b){
+        return b.score - a.score
+    })
+    return arrayList
+}
+
 io.on("connection", socket => {
 
-    socket.on('connected', () => {
+    socket.on('connected', (username) => {
         console.log(`User Connected`)
         clientList[socket.id] = {
-            username: socket.id, 
+            id: socket.id, 
+            username: username, 
+            token: md5(socket.id),
             score: 0
         }
         console.log(clientList)
-        io.emit("userListUpdate", clientList)
+        io.emit("userUpdate", getPlayerList(clientList))
     })
 
     socket.on('pencet', () => {
-        // console.log("pencet");
         const score = clientList[socket.id].score + 1
         clientList[socket.id].score = score
-        io.to(socket.id).emit('hello', score)
+        io.to(socket.id).emit('cokotScore', score)
+        io.emit("userUpdate", getPlayerList(clientList))
     })
 
     socket.on("disconnect", (reason) => {
         console.log("user leave", reason, clientList[socket.id])
-        let flag = 0
-        // Object.keys(clientList).forEach(item => {
-        //     if (clientList[item].username == socket.id) flag = item;
-        // })
-        console.log(flag)
-        clientListTampung = clientList
-
-        clientList.splice(flag, 1)
-        io.emit("userListUpdate", clientList)
+        delete(clientList[socket.id])
+        io.emit("userUpdate", getPlayerList(clientList))
+        console.log("user konek", clientList);
     })
 
 })
